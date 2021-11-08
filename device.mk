d1da10900e8754c5bea459bc4be8ba23c26ad82a
Devin Moore <devinmoore@google.com>
2021-11-08 17:52:47 +0000

    mi439: Switch to new AIDL IR service
    Test: atest VtsHalIrTargetTest hal_implementation_test
    Bug: 205000342
    
    Change-Id: I70c54aa8e8962939cc8f4b42fbbe828e9dad7f0d
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- device.mk
+++ device.mk
@@ -99,8 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Cryptfshw
 TARGET_EXCLUDE_CRYPTFSHW := true
