ad389f77e45e064abe415949e63b2167103c4b9c
Gabriel Biren <gbiren@google.com>
2022-10-28 18:47:03 +0000

    mi439: Switch to AIDL WIFI service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: I4ee61c25dc66e7deee8573e960736d32951b73d2

--- device.mk
+++ device.mk
@@ -331,7 +331,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     libcld80211 \
     libwifi-hal-ctrl \
     libwpa_client \
