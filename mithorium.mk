29559dbf381012fadd79e9583287c1356988f808
Jprimero15 <jprimero155@gmail.com>
2022-12-17 10:55:59 +0800

    mi-common: Add usb to qti components
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- mithorium.mk
+++ mithorium.mk
@@ -366,7 +366,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # QTI Components
-TARGET_COMMON_QTI_COMPONENTS := bt
+TARGET_COMMON_QTI_COMPONENTS += \
+    bt \
+    usb
 
 # Ramdisk
 MITHORIUM_PRODUCT_PACKAGES += \
