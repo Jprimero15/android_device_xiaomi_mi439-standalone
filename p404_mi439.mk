1144d16e11fc2de3f7fba4425ada7596a0d16ff8
Jprimero15 <jprimero155@gmail.com>
2023-03-02 18:58:36 +0800

    mi439: Project not found?
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- p404_mi439.mk
+++ p404_mi439.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq (aospa_mi439, $(TARGET_PRODUCT))
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -21,10 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from mi439 device configuration.
 $(call inherit-product, device/xiaomi/mi439/device.mk)
 
-# Inherit from common AOSPA configuration
-$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+# Inherit from common P-404 configuration
+$(call inherit-product, vendor/404/configs/common.mk)
 
-PRODUCT_NAME := aospa_mi439
+PRODUCT_NAME := p404_mi439
 PRODUCT_DEVICE := mi439
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI SDM439
@@ -40,5 +38,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
-
-endif
