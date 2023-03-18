df65b496541926aa02f4fedc7057f96c73b9b1ff
Jprimero15 <jprimero155@gmail.com>
2023-03-18 15:17:55 +0800

    mi439: Remove more google apps
    * too heavy gapps :/
    * remove google chrome to let the user install their favorite browser (eg. kiwi browser)
    
    Change-Id: Ie7797607c6326905dd9809a88ef5137f099f2a68
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube
+LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
+    Drive \
+    Gmail2 \
+    Maps \
+    Velvet \
+    YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
