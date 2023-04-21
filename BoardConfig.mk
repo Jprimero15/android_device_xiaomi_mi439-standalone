a3048fb1f95f1b75f7edc4bf08169b716015bc06
Jprimero15 <jprimero155@gmail.com>
2023-04-21 08:29:43 +0800

    mi439: Bring back lineage HALs
    Change-Id: I95d29c557e27d771ef35b0d4dacba0932cea616b
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,7 @@ BOARD_SHIPPING_API_LEVEL := 30
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Init
