f03303533555acb631631d83ce3ca2c6ee9296eb
Jprimero15 <jprimero155@gmail.com>
2023-05-01 13:18:11 +0800

    mi439: Import lineage overlays from mithorium
    * exclude proximity and mpctl configs
    * HEAD: https://github.com/Mi-Thorium/android_device_xiaomi_mithorium-common/commit/816161e99288a7e89de70ee9c87e8f5d6b2603f7
    
    Change-Id: Ia49c55ffc69f076aacbc1ce6aa9a953a3aabfbd6
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- device.mk
+++ device.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Overlays RRO
 PRODUCT_PACKAGES += \
     Mi439FrameworksAOSPA \
