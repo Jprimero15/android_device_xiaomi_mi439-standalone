a7fda09e39f00f63af012d560de550c1b8f7370b
Jprimero15 <jprimero155@gmail.com>
2023-05-04 17:50:11 +0800

    mi439: Initial yaap changes?

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,7 +167,7 @@ $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 41943040)) # 40 MB
 
-ifeq ($(TARGET_DISABLES_GMS), true)
+ifneq ($(TARGET_BUILD_GAPPS), true)
 # Partitions - reserved without gapps
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 838860800 # 800 MB
 BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 314572800 # 300 MB
