559fc64820ec85f30469c8dbeb6ba409f170ced4
Jprimero15 <jprimero15@aospa.co>
2023-05-16 13:14:43 +0800

    mi439: Import perf configs from qcom/common
    * DIR: vendor/perf-legacy/configs/msm8937
    * HEAD: https://github.com/AOSPA/android_device_qcom_common/commit/ac3d570e44da2000e7e166efa455ebf7649d885d
    
    Change-Id: I90779802f064e5feb9ac443bc9489d5c6c16f9d3
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- device.mk
+++ device.mk
@@ -255,6 +255,12 @@ PRODUCT_PACKAGES += \
     XiaomiOliveSystemUI \
     XiaomiPine
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
