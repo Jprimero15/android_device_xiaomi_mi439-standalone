93bb65baca3285585158d352143cfbdc8b520d2d
Jprimero15 <jprimero15@aospa.co>
2023-05-29 22:06:11 +0800

    mi439: Move audio to configs dir
    * This directory only includes configurations so move it to its proper place.
    
    Change-Id: I2484b97c8dfa503bcee52d8a0f2e54785c0027f5
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- device.mk
+++ device.mk
@@ -50,7 +50,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
