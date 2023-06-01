29dbe2fb24e1cc58e64e7470bd84d43f58522ac4
Jprimero15 <jprimero15@aospa.co>
2023-06-01 12:49:51 +0800

    mi439: Move *.prop to configs/props/
    * reorganization of dt
    
    Change-Id: Idd34aacc27d7549bd0505a0e3586c388b9d75c1a
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,11 +184,11 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/dt2w"
 
 # Properties
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 ifeq ($(TARGET_KERNEL_VERSION),4.19)
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor_k4.19.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor_k4.19.prop
 endif
 
 # Recovery
