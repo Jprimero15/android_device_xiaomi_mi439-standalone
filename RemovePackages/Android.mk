f81554c4556380afe00c5e333ee03f8103563266
Jprimero15 <jprimero15@aospa.co>
2023-08-11 08:14:44 +0000

    [DO NOT MERGE]mi439: vanilla build
    Change-Id: Iead9a2c877b1126fa9f4e8cc7c5ef990f4cdc75a

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -9,7 +9,8 @@ LOCAL_OVERRIDES_PACKAGES := \
     Drive \
     Gmail2 \
     Maps \
-    YouTube
+    YouTube \
+    QuickSearchBox
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
