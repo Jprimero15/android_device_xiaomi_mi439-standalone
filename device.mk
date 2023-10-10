719cf9041ba666b2f56652c40ef709e1e6ea9d60
Jprimero15 <jprimero15@aospa.co>
2023-10-10 10:28:40 +0800

    [DO NOT MERGE]mi439: Use QTI BT
    * fixes build errors.
    
    Change-Id: I9bc9ac17d267fecc713cbbf719609ba6e642345f

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
-TARGET_USE_QTI_BT_STACK := false
+# TARGET_USE_QTI_BT_STACK := false
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor
