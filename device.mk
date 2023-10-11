5ffed4064ea329df6a78d4ec0e299b72ef034d9e
Jprimero15 <jprimero15@aospa.co>
2023-10-11 17:25:00 +0800

    mi439: Use our wifi 1.6 hidl hal fork
    Change-Id: Ic5d0ca97cec0055e3f8f1fe641a31a053ed94784

--- device.mk
+++ device.mk
@@ -335,7 +335,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.mi439 \
     libcld80211 \
     libwifi-hal-ctrl \
     libwpa_client \
