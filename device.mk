c058e28393d1775700e05c5b1f092904264b2149
Jprimero15 <jprimero15@aospa.co>
2023-10-15 06:05:58 +0800

    mi439: Remove remaining BT packages
    * Let QTI BT component handle this.
    
    Change-Id: I4516e865b98a5153146dddac6568dd073f0d9afb

--- device.mk
+++ device.mk
@@ -24,7 +24,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.primary.msm8937 \
     audio.r_submix.default \
@@ -57,10 +56,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1440
