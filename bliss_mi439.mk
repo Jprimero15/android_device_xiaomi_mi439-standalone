db5e9ead39269f707f0039e6ac49ae69754b072f
Jprimero15 <jprimero15@aospa.co>
2024-03-10 12:36:13 +0800

    mi439: Bliss-ify
    Change-Id: I0a6bbe8270accb20524b626872aa5250f753dfdb

--- bliss_mi439.mk
+++ bliss_mi439.mk
@@ -19,17 +19,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mi439 device configuration.
 $(call inherit-product, device/xiaomi/mi439/device.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from common BLISS configuration
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_mi439
+PRODUCT_NAME := bliss_mi439
 PRODUCT_DEVICE := mi439
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Boot animation resolution.
+# Build flags
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
