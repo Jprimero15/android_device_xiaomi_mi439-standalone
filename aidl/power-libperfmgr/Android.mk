3ce8a860f6b50efd753bc76f47c38b0450535efc
Jprimero15 <jprimero15@aospa.co>
2024-03-11 13:37:12 +0800

    mi439: power-libperfmgr: Do not guard it
    Change-Id: I9378e1d7974f4c9bd0332df51b239964dc3cb1a4

--- aidl/power-libperfmgr/Android.mk
+++ aidl/power-libperfmgr/Android.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-ifneq (,$(findstring hardware/google/interfaces, $(PRODUCT_SOONG_NAMESPACES)))
-ifneq (,$(findstring hardware/google/pixel, $(PRODUCT_SOONG_NAMESPACES)))
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -61,6 +58,3 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_VINTF_FRAGMENTS := android.hardware.power-service.xiaomi.xml
 
 include $(BUILD_EXECUTABLE)
-
-endif
-endif
