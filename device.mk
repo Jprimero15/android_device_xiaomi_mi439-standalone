7284b61e4c9b210964095dee8ce4ebcbacadebc8
Jprimero15 <jprimero15@aospa.co>
2024-03-13 12:03:44 +0800

    mi439: Use task_profiles_30.json for now
    * latest one have uclamp changes which we don't use on default kernel
    
    Change-Id: Ieb6e08cab7e80cab0f38870c455cd76fff3548b7

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ endif
 # Cgroups and task profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Configstore
 PRODUCT_PACKAGES += \
