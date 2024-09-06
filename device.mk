32d0761e9718c559d52e378e932dc2fa8d6b29a5
Jprimero15 <jprimero15@aospa.co>
2024-09-06 23:32:26 +0800

    mi439: Use custom HIDL USB HAL
    Change-Id: I39b1e1e90490a3b0a1e3cbe6d0f57c6d133b9fdf
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- device.mk
+++ device.mk
@@ -392,7 +392,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.basic
+    android.hardware.usb@1.3-service.mi439
 
 # Vibrator
 PRODUCT_PACKAGES += \
