86d10c8328d1da42e9de138e02c43738ebe342cc
Jprimero15 <jprimero15@aospa.co>
2024-09-19 00:03:21 +0800

    [TEMP]Revert "mi439: Use custom HIDL USB HAL"
    This reverts commit 32d0761e9718c559d52e378e932dc2fa8d6b29a5.
    
    Change-Id: I715cdc76ef8921950f79b2d7fefde1f9c45c59b5

--- device.mk
+++ device.mk
@@ -397,7 +397,7 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
-    android.hardware.usb@1.3-service.mi439 \
+    android.hardware.usb@1.3-service.basic \
     android.hardware.usb.gadget@1.2-service-qti
 
 # Vibrator
