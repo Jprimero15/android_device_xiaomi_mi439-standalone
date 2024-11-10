4aa52988ad18fae111009c273d7e2410e73d55c4
Jprimero15 <jprimero15@aospa.co>
2024-11-10 09:54:55 +0800

    Revert "mi439: Build missing libraries for 14 QPR3"
    This reverts commit d9622bf81c200030b9cf0959544ad1aed1acda88.

--- device.mk
+++ device.mk
@@ -281,11 +281,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-# QMI
-PRODUCT_PACKAGES += \
-    libnetutils.vendor:64 \
-    libsqlite.vendor:64 \
-
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS += \
     alarm \
@@ -315,7 +310,6 @@ PRODUCT_AAPT_PREF_CONFIG ?= xhdpi
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    libpower.vendor:64 \
     libsensorndkbridge
 
 # Shims
