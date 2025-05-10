87d79170edbd7dd5c7073179f25da5622646268c
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-10 17:58:53 +0800

    mi439: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,6 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_sdm439
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_sdm439
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
